236d5c5106d6e2e1b0d54c7859b00d2ce7dffe63
Pranjalsanandiya123 <64524085+Pranjalsanandiya123@users.noreply.github.com>
2020-05-09 12:32:44 +0530

    Implemented Generics in InMemoryRepository

---
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,81 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T> where T: BaseEntity//incoming class name
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;   //List by class properties
+        string className; //variable for storing incoming classname 
+
+        public InMemoryRepository()
+        {
+            className = typeof(T).Name;   //className takes the name of provided classname by T
+            items = cache[className] as List<T>; //Checks whether something is inside cache or not
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+        
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.Id == t.Id);
+            if (tToUpdate!=null)
+            {
+                tToUpdate = t;
+            }
+            else
+            {
+                throw new Exception(className + " not found!!");
+            }
+        }
+        public T Find(string Id)
+        {
+            T t = items.Find(i => i.Id == Id);
+            if (t != null)
+            {
+                return t;
+            }
+            else
+            {
+                throw new Exception(className + " not found!!");
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            T tToDelete = items.Find(i => i.Id == Id);
+            if (tToDelete != null)
+            {
+                items.Remove(tToDelete);
+            }
+            else
+            {
+                throw new Exception(className + " not found!!");
+            }
+        }
+    }
+}
